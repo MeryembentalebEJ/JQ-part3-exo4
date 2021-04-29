4204ff51887b0a28f0cc66027bd1aff97286373b
Meryembentalebeljadil <meryembentaleb11@gmail.com>
2021-04-29 16:53:35 +0200

    Click a button change the color

---
+++ app.js
@@ -0,0 +1,12 @@
+
+  $(document).ready(function(){
+    $("#green").click(function(){
+        $("#texte").css('color','#38a758');
+    });
+    $("#red").click(function(){
+        $("#texte").css('color','#a70d26');
+    });
+    $("#blue").click(function(){
+        $("#texte").css('color','#0b99bf'); //hide
+    });
+  });
